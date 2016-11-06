85b3200d1928d9ed6cdd98f511d5b436217e8e65
grance1 <2397580855@qq.com>
2016-11-06 22:39:14 +0800

    while

---
+++ 2016-11-1/dic.py
@@ -0,0 +1,20 @@
+d = {'Adam':95,
+     'Lisa':85,
+     'Bart':59}
+print "Adam:", d['Adam']
+print "Lisa:",d['Lisa']
+print "Bart:",d['Bart']
+print d['Adam']
+print
+print
+print d
+print len(d)
+print d['Adam']
+print d.get('Paul')
+d = {'Adam':95,
+     'Lisa':85,
+     'Bart':59,
+     'Paul':75}
+print d
+print len(d)
+print d.get('Bart')
